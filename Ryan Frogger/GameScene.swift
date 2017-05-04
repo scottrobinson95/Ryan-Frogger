980f2a91bede7d6a006a6b831a802936b8b0993f
Student <Student@Students-MacBook-Air.local>
2017-05-04 13:23:27 -0500

    Lane of cars and a frog that disappears when it hits a car

--- Ryan Frogger/GameScene.swift
+++ Ryan Frogger/GameScene.swift
@@ -20,15 +20,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         physicsWorld.contactDelegate = self
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         unit = frame.height/15
-        frog.physicsBody?.affectedByGravity = false
-        frog.position = CGPoint(x: frame.midX, y: frame.minY+unit)
-        frog.size = CGSize(width: unit, height: unit)
-        frog.anchorPoint = CGPoint(x: 0.5, y: 1.0)
-        addChild(frog)
-        makeCars(row: 5, speed: 10.0, fromRight: true, carWidth: 2)
+        makeFrog()
+        makeLane(laneRow: 3, laneSpeed: 10.0, laneFromRight: true, laneCarWidth: 30, interval: 2.0)
         
     }
     
+    func didBegin(_ contact: SKPhysicsContact)
+    {
+        print("splat")
+        if contact.bodyA.node?.name == "car" || contact.bodyB.node?.name == "car"
+        {
+            frog.removeFromParent()
+        }
+    }
     
     func touchDown(atPoint pos : CGPoint) {
     }
@@ -63,51 +67,79 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         // Called before each frame is rendered
     }
     
+    func makeFrog()
+    {
+        frog.physicsBody?.affectedByGravity = false
+        frog.position = CGPoint(x: frame.midX, y: frame.minY+unit)
+        frog.size = CGSize(width: unit-5, height: unit-5)
+        frog.physicsBody = SKPhysicsBody(rectangleOf: frog.size)
+        frog.physicsBody?.usesPreciseCollisionDetection = true
+        frog.physicsBody?.isDynamic = true
+        frog.physicsBody?.affectedByGravity = false
+        
+        addChild(frog)
+    }
+    
     func makeCars(row: CGFloat, speed: TimeInterval, fromRight: Bool, carWidth: CGFloat)
     {
         let car = SKSpriteNode(color: UIColor.red, size: CGSize(width: unit, height: unit))
         car.name = "car"
         car.size = CGSize(width: unit*carWidth, height: unit)
+        car.physicsBody = SKPhysicsBody(rectangleOf: car.size)
+        car.physicsBody?.affectedByGravity = false
+        car.physicsBody?.isDynamic = false
+        car.physicsBody?.contactTestBitMask = (car.physicsBody?.collisionBitMask)!
         if fromRight == true
         {
-            car.anchorPoint = CGPoint(x: 0.0, y: 1.0)
-            car.position = CGPoint(x: frame.midX, y: frame.minY+row*(unit))
-            let scroll = SKAction.moveBy(x: -frame.width, y: 0, duration: speed)
+            car.position = CGPoint(x: frame.maxX, y: frame.minY+row*(unit))
+            let scroll = SKAction.moveBy(x: -frame.width*1.5, y: 0, duration: speed)
             addChild(car)
             car.run(scroll)
 
         }
         if fromRight == false
         {
-            car.anchorPoint = CGPoint(x: 1.0, y: 1.0)
-            car.position = CGPoint(x: frame.midX, y: frame.minY+row*(unit))
-            let scroll = SKAction.moveBy(x: frame.width, y: 0, duration: speed)
+            car.position = CGPoint(x: frame.minX, y: frame.minY+row*(unit))
+            let scroll = SKAction.moveBy(x: frame.width*1.5, y: 0, duration: speed)
             addChild(car)
             car.run(scroll)
 
         }
     }
+    
     func makeLogs(row: CGFloat, speed: TimeInterval, fromRight: Bool, logWidth: CGFloat)
     {
-        let log = SKSpriteNode(color: UIColor.red, size: CGSize(width: unit, height: unit))
+        let log = SKSpriteNode(color: UIColor.brown, size: CGSize(width: unit, height: unit))
         log.name = "log"
         log.size = CGSize(width: unit*logWidth, height: unit)
+        log.physicsBody = SKPhysicsBody(rectangleOf: log.size)
+        log.physicsBody?.affectedByGravity = false
+        log.physicsBody?.isDynamic = true
+        log.physicsBody?.contactTestBitMask = (log.physicsBody?.collisionBitMask)!
+
         if fromRight == true
         {
-            log.anchorPoint = CGPoint(x: 0.0, y: 1.0)
             log.position = CGPoint(x: frame.maxX, y: row*(unit))
-            let scroll = SKAction.moveBy(x: -frame.width, y: 0, duration: speed)
+            let scroll = SKAction.moveBy(x: -frame.width*1.5, y: 0, duration: speed)
             addChild(log)
             log.run(scroll)
         }
         if fromRight == false
         {
-            log.anchorPoint = CGPoint(x: 1.0, y: 1.0)
-            log.position = CGPoint(x: frame.minX, y: row*(unit))
+            log.position = CGPoint(x: frame.minX*1.5, y: row*(unit))
             //let scroll = SKAction.moveBy(x: frame.width, y: 0, duration: speed)
             addChild(log)
             //log.run(scroll)
         }
         addChild(log)
+    }
+    
+    func makeLane(laneRow: CGFloat, laneSpeed: TimeInterval, laneFromRight: Bool, laneCarWidth: CGFloat, interval: TimeInterval)
+    {
+        let carTimer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(self.updateCounter), userInfo: nil, repeats: true)
+    }
+    func updateCounter()
+    {
+        makeCars(row: 5, speed: 5.0, fromRight: true, carWidth: 2)
     }
 }
