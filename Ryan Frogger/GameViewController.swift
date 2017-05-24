cd6c66d673daf6e462cb2c2e28f6dc91fd8ec4de
Student <Student@Students-MacBook-Air.local>
2017-05-24 08:55:37 -0500

    final commit

--- Ryan Frogger/GameViewController.swift
+++ Ryan Frogger/GameViewController.swift
@@ -29,7 +29,7 @@ class GameViewController: UIViewController {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .aspectFit
                 
                 // Present the scene
                 view.presentScene(scene)
